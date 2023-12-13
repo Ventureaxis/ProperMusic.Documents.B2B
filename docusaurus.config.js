d02edcfee84c4d1429b5fa1ae77a5e4777dab913
Jakob Camwell <jakob.camwell@ventureaxis.com>
2023-12-13 09:07:41 +0000

    Config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,19 +11,19 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://b2b.properweb.co.uk/',
+    url: 'https://ventureaxis.github.io/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'ProperMusicGroup', // Usually your GitHub org/user name.
-    projectName: 'propermusic.documents.b2b', // Usually your repo name.
-
+    organizationName: 'VentureAxis', // Usually your GitHub org/user name.
+    projectName: 'ProperMusic.Documents.B2B', // Usually your repo name.
+    deploymentBranch: 'deployment',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-
+    trailingSlash: false,
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
